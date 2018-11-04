2b9745bcc9e010d52c6890f0126d24e12d81213d
ladislavbili <l.bilisics@gmail.com>
2018-11-04 20:38:10 +0100

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyBEtoMankEdsQyhtAG2gOsYtU9kkj0zR3Y",
-  authDomain: "striebrosrsty.firebaseapp.com",
-  databaseURL: "https://striebrosrsty.firebaseio.com",
-  projectId: "striebrosrsty",
-  storageBucket: "striebrosrsty.appspot.com",
-  messagingSenderId: "946125128927"
-};
